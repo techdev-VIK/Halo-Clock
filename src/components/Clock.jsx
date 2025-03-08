8fbdfbb4311ba5e854607fa2264d9fda9490d28c
Vikartan Sood <139503457+techdev-VIK@users.noreply.github.com>
2025-03-08 16:26:17 +0530

    Clock sync with Date

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -3,19 +3,20 @@ import { useEffect, useState } from "react"
 
 function Clock() {
 
-  const [time, setTime] = useState(new Date().toLocaleDateString());
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
 
-  const [secondsRotation, setSecondsRotation] = useState(0);
+  const [secondsRotation, setSecondsRotation] = useState(90);
 
-  const [minutesRotation, setMinutesRotation] = useState(0);
+  const [minutesRotation, setMinutesRotation] = useState(90);
 
-  const [hoursRotation, setHoursRotation] = useState(0);
+  const [hoursRotation, setHoursRotation] = useState(90);
 
 
 
   useEffect(() => {
+    setTime(new Date().toLocaleTimeString())
+
     const interval = setInterval(() => {
-        setTime(new Date().toLocaleDateString());
         setSecondsRotation(rotation => rotation + 6);
     }, 1000);
 
@@ -34,11 +35,19 @@ function Clock() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-        setHoursRotation(rotation => rotation + 30);
+        setHoursRotation(rotation => rotation + 15);
     }, 3600000);
 
     return () => clearInterval(interval)
   }, []);
+
+
+
+  const currentTime = new Date();
+
+  const currentSecond = currentTime.getSeconds();
+  const currentMinute = currentTime.getMinutes();
+  const currentHour = currentTime.getHours();
 
    const secondsRing = () => {
 
@@ -49,11 +58,11 @@ function Clock() {
       }
 
       return arr.map((second, index) => {
+        const isCurrentSecond = index + 1 === currentSecond;
         
-
         return(
-            <span key={index} style={{ transform: `rotate(${secondsRotation + index * 6}deg)` }}>
-                <b style={{ transform: `rotate(${-index * 6}deg)` }}>{second}</b>
+            <span key={index} style={{ transform: `rotate(${secondsRotation + index * 6}deg)`, transition: "transform 1s linear" }}>
+                <p style={{ display: "inline-block", transform: `rotate(${-secondsRotation -index * 6}deg)`, color: isCurrentSecond ? "aqua" : "gray"}}>{second}</p>
             </span>
         )
       })
@@ -69,9 +78,12 @@ function Clock() {
       }
 
     return arr.map((minutes, index) => {
+        const isCurrentMinute = index + 1 === currentMinute;
         return (
             <span key={index} style={{ transform: `rotate(${minutesRotation + index * 6}deg)` }}>
-                {minutes}
+                <p style={{display:"inline-block", transform: `rotate(${-minutesRotation -index * 6}deg)`, color: isCurrentMinute ? "aqua": "gray"}}>
+                    {minutes}
+                </p>
             </span>
         )
     })
@@ -87,9 +99,12 @@ function Clock() {
       }
 
     return arr.map((hours, index) => {
+        const isCurrentHour = index + 1 === currentHour;
         return (
             <span key={index} style={{ transform: `rotate(${hoursRotation + index * 15}deg)` }}>
-                {hours}
+                <p style={{display: "inline-block", transform:`rotate(${-hoursRotation -index*15}deg)`, color:isCurrentHour ? "aqua": "lightgray"}}>
+                    {hours}
+                </p>
             </span>
         )
     })
@@ -105,7 +120,10 @@ function Clock() {
         <div className="seconds-ring">
             {minutesRing()}
             <div className="hours-ring">
-                    {hoursRing()}
+                {hoursRing()}
+                <div className="current-timer">
+                    {time}
+                </div>
             </div>
         </div>
       </div>
